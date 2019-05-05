df1292ad4e57077e3c72a87005a6c710a39d11f9
vhumphrey <vhumphrey@users.noreply.github.com>
2019-05-05 18:48:22 -0400

    note added

---
+++ apps/01_hello_world/you_try/program.py
@@ -0,0 +1,14 @@
+print('-----------------------------')
+print('        Hello App')
+print('-----------------------------')
+print()
+
+# TODO: This fine
+
+user_text = input('What is your name? ')
+greeting = 'Nice to meet you ' + user_text
+
+print(greeting)
+
+
+
